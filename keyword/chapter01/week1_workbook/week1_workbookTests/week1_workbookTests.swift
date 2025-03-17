0a7dd1ba3bdb390a7210a8246a6a14473577b9a0
ChangWoo <kilolo0@naver.com>
2025-03-17 17:12:52 +0900

    [workbook] week01 practice submit

---
+++ keyword/chapter01/week1_workbook/week1_workbookTests/week1_workbookTests.swift
@@ -0,0 +1,17 @@
+//
+//  week1_workbookTests.swift
+//  week1_workbookTests
+//
+//  Created by 김창우 on 3/16/25.
+//
+
+import Testing
+@testable import week1_workbook
+
+struct week1_workbookTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
