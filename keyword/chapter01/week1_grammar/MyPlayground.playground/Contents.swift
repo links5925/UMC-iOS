7e7f815207a74a705e33989dfd4a5591f9edc6d1
ChangWoo <kilolo0@naver.com>
2025-03-17 20:17:46 +0900

    [workbook] week01 grammar workbook submit

---
+++ keyword/chapter01/week1_grammar/MyPlayground.playground/Contents.swift
@@ -0,0 +1,22 @@
+import UIKit
+
+let name:String = "UMC"
+
+var age:Int = 10
+
+print("이름: \(name)")
+print("나이: \(age)")
+
+
+let numA: Int = 10
+let numB: Double = 10.2
+
+let sum = numA + Int(numB)
+print("결과 : \(sum)")
+
+
+let name2:String = "Jeong"
+
+var age2:Int = 28
+
+print("이름: \(name2), 나이: \(age2)")
